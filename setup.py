bdbddf421eeb5fb81b416662470fd30db48259eb
matsumoto.kodai <matsumoto.kodai@linecorp.com>
2019-07-05 12:45:41 +0900

    Fix setup file

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
-    name='python3-logstash',
+    name='python3-http-logstash',
     packages=['logstash'],
-    version='0.4.73',
+    version='0.4.74',
     description='Python logging handler for Logstash.',
-    long_description=open('README.md').read(),
+    #long_description=open('README.md').read(),
     author='Israel Flores',
-    author_email='jobs@israelfl.com',
-    url='https://github.com/israel-fl/python3-logstash',
+    author_email='frauniki@sinoa.jp',
+    url='https://github.com/frauniki/python3-logstash',
     classifiers=[
-        'Development Status :: 1 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Logging',
     ]
 )
